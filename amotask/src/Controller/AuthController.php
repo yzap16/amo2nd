2cd40d414d32d3e9702401d8ffe32fcccfc80b5e
YZap <yzap@example.com>
2025-05-28 09:55:00 +0000

    AuthController refactor

--- amotask/src/Controller/AuthController.php
+++ amotask/src/Controller/AuthController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Exception\AmoCrmApiException;
 use App\Service\AuthService;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -11,20 +13,61 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class AuthController extends AbstractController
 {
-    private AuthService $authService;
-    
-    public function __construct(AuthService $authService) {
-        $this->authService = $authService;
-    }
+    public function __construct(
+        private AuthService $authService,
+        private LoggerInterface $logger
+    ) { }
     
     #[Route('/auth/login', name: 'app_auth')]
     public function login(): Response
     {
-        $this->authService->login();
-           
-        return $this->json([
-            'status' => 'success',
-            'message' => 'Вы успешно залогинились'
-        ], 200);
+        try {
+            $this->authService->login();
+
+            return $this->json([
+                'status' => 'success',
+                'message' => 'Вы успешно залогинились'
+            ], 200);
+        }
+        catch (ClientException $e) {
+            
+            $errorResponse = $e->getResponse();
+            $errorDetails = $errorResponse->toArray(false);
+
+            $this->logError('Ошибка клиента', ['exception' => $e]);
+            
+            return $this->json([
+                'status' => 'server_error',
+                'errors' => $e->getMessage(),
+                'message' => 'Кажется, что-то пошло не так. Повторите свой запрос позднее.'
+            ], 500);
+
+        }
+        catch (AmoCrmApiException $e) {
+            
+            $this->logError('Ошибка API', ['exception' => $e]);
+            
+            return $this->json([
+                'status' => 'api_error',
+                'errors' => $e->getMessage(),
+                'message' => 'Кажется, что-то пошло не так...'
+            ], 500);
+
+        }
+        catch (\Throwable $e) {
+
+            $this->logError('Ошибка', ['exception' => $e]);
+            
+            return $this->json([
+                'status' => 'server_error',
+                'errors' => $e->getMessage(),
+                'message' => 'Кажется, что-то пошло не так...'
+            ], 500);
+            
+        }
+    }
+
+    private function logError(string $message, array $errors) {
+        return $this->logger->error($message, $errors);
     }
 }
