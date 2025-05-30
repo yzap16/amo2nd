f62e96b83a63abe1459b7fa4fc4cf63b762b4df4
YZap <yzap@example.com>
2025-05-30 06:58:14 +0000

    Is product catalog available?

--- amotask/src/Service/LeadService.php
+++ amotask/src/Service/LeadService.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Service;
 
 use App\Adapter\Interface\IAmoCrmLead;
+use App\Adapter\Interface\IAmoCrmProduct;
 use App\Service\AuthService;
 use App\Service\ProductService;
 use App\Service\TaskService;
@@ -15,7 +16,8 @@ final class LeadService
         private AuthService $authService,
         private ProductService $productService,
         private TaskService $taskService,
-        private IAmoCrmLead $leadAdapter
+        private IAmoCrmLead $leadAdapter,
+        private IAmoCrmProduct $productAdapter
     ) { }
     
     public function create(int $contactId): void {
@@ -25,14 +27,38 @@ final class LeadService
         $response = $this->leadAdapter->createAmoCrmLead($contactId);
         
         if ($response->getStatusCode() === 200 || $response->getStatusCode() === 201) {
+            
             $result = $response->toArray();
             $leadId = $result['_embedded']['leads'][0]['id'];
 
             $this->taskService->create($leadId);
-            $this->productService->createProducts($leadId);
+
+            $this->productAdapter->setAccessToken($this->authService->getAccessToken());
+            if ($this->isProductCatalogAvailable()) {
+                $this->productService->createProducts($leadId);
+            }
+
         } else {
             throw new AmoCrmApiException($response->getContent(false));
         }
+    }
+
+    private function isProductCatalogAvailable(): bool {
+
+        $response = $this->productAdapter->getCatalogs();
+
+        if ($response->getStatusCode() === 200) {
+            foreach ($response->toArray()['_embedded']['catalogs'] as $catalog) {
+                if ($catalog['type'] === 'products') {
+                    return true;
+                }
+            }
+            return false;
+        }
+        else {
+            throw new AmoCrmApiException($response->getContent(false));
+        }
+
     }
 
 }
